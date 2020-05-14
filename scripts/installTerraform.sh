0b0c74ab14fb19489a86507f93fbbecde2d3e377
ang <andy.ng@digicatapult.org.uk>
2020-05-14 13:02:45 +0100

    shellscript

--- scripts/installTerraform.sh
+++ scripts/installTerraform.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 sudo apt-get install wget unzip
 cd ~/
 sudo wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
-sudo unzip ./terraform_0.12.2_linux_amd64.zip –d /usr/local/bin
+sudo unzip ./terraform_0.12.2_linux_amd64.zip -d /usr/local/bin
 
