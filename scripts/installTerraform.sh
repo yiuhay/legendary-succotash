7e90f46bedaa1193d0401a13e65c35570c2e3e28
ang <andy.ng@digicatapult.org.uk>
2020-05-14 12:38:20 +0100

    bash script

---
+++ scripts/installTerraform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install wget unzip
+sudo wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
+sudo unzip ./ terraform_0.12.2_linux_amd64.zip –d /usr/local/bin
+
